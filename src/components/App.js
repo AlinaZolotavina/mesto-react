1e98bb86e853f4335dbd4403b5327d189e8bf884
Alina Zolotavina <abekmetova@yandex.ru>
2022-05-21 18:52:07 +0400

    feat: open and close popus(edit profile, change avatar, add photo)

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,108 @@ import React, { useState } from 'react';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
+import PopupWithForm from './PopupWithForm';
 
 function App() {
+  const [isEditProfilePopupOpen, setEditProfilePopupOpen] = useState(false);
+  const [isAddPhotoPopupOpen, setAddPhotoPopupOpen] = useState(false);
+  const [isConfirmationPopupOpen, setConfirmationPopupOpen] = useState(false);
+  const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = useState(false);
+
+  function handleEditProfilePopupOpen() {
+    setEditProfilePopupOpen(!isEditProfilePopupOpen);
+  }
+
+  function handleAddPhotoFormPopupOpen() {
+    setAddPhotoPopupOpen(!isAddPhotoPopupOpen);
+  }
+
+  function handleConfirmationPopupOpen() {
+    setConfirmationPopupOpen(!isConfirmationPopupOpen);
+  }
+
+  function handleEditAvatarPopupOpen() {
+    setEditAvatarPopupOpen(!isEditAvatarPopupOpen);
+  }
+
+  function closeAllPopups() {
+    setEditProfilePopupOpen(false);
+    setAddPhotoPopupOpen(false);
+    setConfirmationPopupOpen(false);
+    setEditAvatarPopupOpen(false);
+  }
+
   return (
     <div className="page__container">
       <Header className="header" />
-      <Main />          
+      <Main 
+        className="content"
+        onEditProfile={handleEditProfilePopupOpen}
+        onAddPlace={handleAddPhotoFormPopupOpen}
+        onEditAvatar={handleEditAvatarPopupOpen}
+      />          
       <Footer className="footer" />
+
+      <PopupWithForm
+        name="edit-profile"
+        title="Редактировать профиль"
+        formName="editProfileForm"
+        buttonText="Сохранить"
+        isOpen={isEditProfilePopupOpen}
+        onClose={closeAllPopups}
+      >
+        <label className="form__item">
+          <input className="form__input form__input_type_name" id="name-input" type="text" name="name" placeholder="Ваше имя" minLength="2" maxLength="40" required />
+          <span className="form__input-error name-input-error">Вы пропустили это поле.</span>
+        </label>
+        <label className="form__item">
+          <input className="form__input form__input_type_job" id="job-input" type="text" name="about" placeholder="Кратко о Вас" minLength="2" maxLength="200" required />
+          <span className="form__input-error job-input-error">Вы пропустили это поле.</span>
+        </label>
+      </PopupWithForm>
+
+      <PopupWithForm
+        name="add-photo"
+        title="Новое место"
+        formName="addNewPhotoForm"
+        buttonText="Создать"
+        isOpen={isAddPhotoPopupOpen}
+        onClose={closeAllPopups}
+      >
+        <label className="form__item">
+          <input className="form__input form__input_type_title" id="title-input" type="text" name="name" placeholder="Название" minLength="2" maxLength="30" required />
+          <span className="form__input-error title-input-error">Вы пропустили это поле.</span>
+        </label>
+        <label className="form__item">
+          <input className="form__input form__input_type_link" id="card-link-input" type="url" name="link" placeholder="Ссылка на картинку" required />
+          <span className="form__input-error card-link-input-error">Введите адрес сайта.</span>
+        </label>
+      </PopupWithForm>
+
+      <PopupWithForm
+        name="confirmation"
+        title="Вы уверены?"
+        formName="confirmForm"
+        buttonText="Да"
+        isOpen={isConfirmationPopupOpen}
+        onClose={closeAllPopups}
+      >          
+      </PopupWithForm>
+
+      <PopupWithForm
+        name="avatar-edit"
+        title="Обновить аватар"
+        formName="updateAvatar"
+        buttonText="Сохранить"
+        isOpen={isEditAvatarPopupOpen}
+        onClose={closeAllPopups}
+      >
+        <label className="form__item">
+          <input className="form__input form__input_type_link" id="avatar-link-input" type="url" name="avatar" placeholder="Ссылка на картинку" required />
+          <span className="form__input-error avatar-link-input-error">Ссылка на зображение.</span>
+        </label>
+      </PopupWithForm>
+              
     </div>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,43 @@
 import React from 'react';
+import api from '../utils/api';
 
-function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick, onDeleteBtnClick }) {
+function Main({ onEditAvatar, onEditProfile, onAddPlace }) {
+
+  const [userAvatar, setUserAvatar] = React.useState('');
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+
+  React.useEffect(() => {
+    api.getUserData()
+    .then(data => {
+      const userData = data;
+      setUserAvatar(userData.avatar);
+      setUserName(userData.name);
+      setUserDescription(userData.about);
+    })
+    .catch((err) => console.log(err));
+  }, []);
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar">
-          <img className="profile__avatar-image" src="<%=require('./images/avatar.jpg')%>" alt="фото профиля" />
+          <div className="profile__avatar-image" style={{ backgroundImage: `url(${userAvatar})` }} />
           <div className="profile__darkening"></div>
-          <div className="profile__avatar-edit-btn"></div>
+          <div className="profile__avatar-edit-btn" onClick={onEditAvatar}></div>
         </div>
         <div className="profile__info">
           <div className="profile__info-user">
-            <h1 className="profile__name">Жак-Ив Кусто</h1>
-            <button className="profile__edit-btn"></button>
+            <h1 className="profile__name">{userName}</h1>
+            <button className="profile__edit-btn" onClick={onEditProfile}></button>
           </div>
-          <p className="profile__job">Исследователь океана</p>
+          <p className="profile__job">{userDescription}</p>
         </div>
-        <button className="profile__add-btn"></button>
+        <button className="profile__add-btn" onClick={onAddPlace}></button>
       </section>
-
       <section className="elements">
-        <ul className="elements__list"></ul>
+        <ul className="elements__list">
+        </ul>
       </section>
     </main>
   )
